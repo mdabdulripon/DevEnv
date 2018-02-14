fb96d6e035a9f65a733d042745284ad023340c94
Md Abdul A ripon <ripon@Mds-MacBook-Pro.local>
2018-02-14 12:24:35 -0500

    add build tesk.

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
-var $ = require('jquery');
+window.$ = window.jQuery = require('jquery')
+    // require('tether');
+    // require('bootstrap')
+    // require('../node_modules/tether/dist/js/tether.min');
+require('../node_modules/bootstrap/dist/js/bootstrap.min')
 
 $(function() {
     alert("Here I'm. This is cool exprience!");
