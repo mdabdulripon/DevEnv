7af47297b5fdb217dc03c13f6816c5b22b9f3398
Md Abdul A ripon <ripon@Mds-MacBook-Pro.local>
2018-03-07 12:04:58 -0500

    add browsersync with webpack

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,7 @@
 import webpack from 'webpack';
 import path from 'path';
 import htmlWebpackPlugin from 'html-webpack-plugin';
+import BrowserSyncPlugin from 'browser-sync-webpack-plugin';
 
 
 
@@ -15,7 +16,30 @@ export default {
         publicPath: '/',
         filename: 'bundle.js'
     },
+    watch: true,
     plugins: [
+        new BrowserSyncPlugin(
+            // BrowserSync options
+            {
+                // browse to http://localhost:3000/ during development
+                host: 'localhost',
+                port: 3000,
+                files: [
+                    'src/**/*',
+
+                ],
+                // proxy the Webpack Dev Server endpoint
+                // (which should be serving on http://localhost:3100/)
+                // through BrowserSync
+                proxy: 'http://localhost:3001/'
+            },
+            // plugin options
+            {
+                // prevent BrowserSync from reloading the page
+                // and let Webpack Dev Server take care of this
+                reload: false
+            }
+        ),
         /**
          * ? html-webpack-plugin
          * * Copy Html to the dist folder
