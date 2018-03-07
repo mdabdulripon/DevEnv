25ce094b51562a9cedd7d15bd242aaab929daffa
Md Abdul A ripon <ripon@Mds-MacBook-Pro.local>
2018-03-07 12:14:56 -0500

    add color variables and cleanup browser-synce plagin

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -18,28 +18,25 @@ export default {
     },
     watch: true,
     plugins: [
-        new BrowserSyncPlugin(
-            // BrowserSync options
-            {
-                // browse to http://localhost:3000/ during development
+        new BrowserSyncPlugin({
+                /**
+                 * ! browse to http://localhost:3000/ during development
+                 * ? proxy the Webpack Dev Server endpoint.
+                 * * which should be serving on http://localhost:3001/
+                 * */
                 host: 'localhost',
                 port: 3000,
                 files: [
                     'src/**/*',
-
                 ],
-                // proxy the Webpack Dev Server endpoint
-                // (which should be serving on http://localhost:3100/)
-                // through BrowserSync
                 proxy: 'http://localhost:3001/'
             },
-            // plugin options
+            /**
+             * * plugin options
+             * */
             {
-                // prevent BrowserSync from reloading the page
-                // and let Webpack Dev Server take care of this
                 reload: false
-            }
-        ),
+            }),
         /**
          * ? html-webpack-plugin
          * * Copy Html to the dist folder
